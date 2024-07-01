b0b158332c4c9838c7ec18ed3a10b4a2519aae1e
Ahmet <ahmethatipk99@gmail.com>
2024-07-01 14:14:34 +0300

    Database oluşturuldu

---
+++ EasyCashIdentityProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,22 @@
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DataAccessLayer.Concrete
+{
+    public class Context : IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-AA94TK2\\SQLEXPRESS; initial catalog=EasyCashDb; integrated Security= true");
+        }
+
+        public DbSet<CostumerAccount> CostumerAccounts { get; set; }
+        public DbSet<CostumerAccountProcess> CostumerAccountProcesses { get; set; }
+    }
+}
---
+++ EasyCashIdentityProject.EntityLayer/Concrete/CostumerAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.EntityLayer.Concrete
+{
+    public class CostumerAccount
+    {
+        public int CostumerAccountID { get; set; }
+        public string CostumerAccountNumber { get; set; }
+        public string CostumerAccountCurrency { get; set; }
+        public decimal CostumerAccountBalance { get; set; }
+        public string BankBranch { get; set; }
+    }
+}
